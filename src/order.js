4dd36abdfe867e4680dc025b7cfcd120dd9a13da
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-05-13 17:10:43 -0600

    Complete order functions

--- src/order.js
+++ src/order.js
@@ -1,7 +1,40 @@
+function takeOrder(order, orders) {
+  if (orders.length < 3) {
+    return orders.push(order)
+  } else {
+    return
+  }
+}
+
+function refundOrder(orderNumber, orders) {
+  for (var i = 0; i < orders.length; i++) {
+    if (orderNumber === orders[i].orderNumber) {
+        orders.splice(i, 1)
+    }
+  }
+}
+
+function listItems(orders) {
+  var names = []
+  for (var i = 0; i < orders.length; i++) {
+    names.push(orders[i].item)
+  }
+  return names.join(", ")
+}
+
+function searchOrder(orders, item) {
+  var count = 0;
+  for (var i = 0; i < orders.length; i++) {
+    if (orders[i].item === item) {
+      count++
+    }
+  }
+  return count > 0;
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
