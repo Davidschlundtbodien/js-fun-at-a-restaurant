40f075c40eaf9c2749ac28a900f974526c34691b
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-05-13 16:22:14 -0600

    Complete meal functions

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 }
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  }
+}
+
+function addIngredients(ingredient, array) {
+  if (array.includes(ingredient)) {
+    return
+  } else {
+    array.push(ingredient)
+  }
+  return array
+}
+
+function formatPrice(price) {
+  return `$${price}`
+}
+
+function decreasePrice(price) {
+  var newPrice = price*.9
+  return newPrice
+}
+
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+  return recipe;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
