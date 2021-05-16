dac75479d09f72eb5120e9e5f4a06f6e46d1a770
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-05-15 21:17:07 -0600

    Complete Chef Class

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,22 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name,
+    this.restaurant = restaurant
+  }
+  greetCustomer(customer, morning) {
+    if (morning) {
+      return `Good morning, ${customer}!`
+    } else {
+      return `Hello, ${customer}!`
+    }
+
+  }
+  checkForFood(foodItem) {
+    if (this.restaurant.menus.breakfast.includes(foodItem)) {
+      return `Yes, we're serving ${foodItem.name} today!`
+    } else {
+      return `Sorry, we aren't serving ${foodItem.name} today.`
+    }
   }
 }
 
